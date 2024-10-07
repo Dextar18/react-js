7dc4c5a546ee011a86821459467e4bc8a316ad43
Dextar18 <141416359+Dextar18@users.noreply.github.com>
2024-10-07 15:06:54 +0530

    Add files via upload

---
+++ src/Classcom/Com.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import './index.css'
+export default function Com() {
+  return (
+    <div className='nav-1'>
+                <a href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+    </div>
+  )
+}
